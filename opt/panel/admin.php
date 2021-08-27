2af9cb45fb8a90ed9d74cf1b016d5c9fff4f57ac
SirDankenstien <52797753+SirDankenstien@users.noreply.github.com>
2021-08-27 18:43:46 +0530

    .

--- opt/panel/admin.php
+++ opt/panel/admin.php
@@ -273,7 +273,7 @@ if(isset($_POST['action'])) {
 					
 					<div class="controls">
 						<select name="version" id="version" class="span4">
-							<option value="Paper">PaperMC</option>
+							<option value="1.17.1">PaperMC</option>
 						</select>
 					</div>
 				</div>
